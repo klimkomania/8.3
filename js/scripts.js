8426e71b155227de04bbbc5c273a4d18afffe809
klimkomania <klimkomania@interia.pl>
2017-04-06 12:57:43 +0200

    dokonano poprawek na zlecenie mentora

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 var name = prompt('Wpisz swoje imię');
-//alert('Witaj, ' + name);
+// alert('Witaj, ' + name);
+/*
 console.log('Witaj, ' + name);
+console.log('Witaj, Panie kolego o imieniu' + name);
+*/
+console.log('Witaj, Panie kolego starszy o imieniu ' + name);
